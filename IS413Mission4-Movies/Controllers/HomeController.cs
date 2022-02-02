c954eeb3955373813fb6e003d02ebcdbad0aa512
Josh Moody <joshmoody24@gmail.com>
2022-02-01 21:46:01 -0700

    Added update and delete functionality

--- IS413Mission4-Movies/Controllers/HomeController.cs
+++ IS413Mission4-Movies/Controllers/HomeController.cs
@@ -34,6 +34,7 @@ namespace IS413Mission5_Movies.Controllers
         public IActionResult AddMovie()
         {
             ViewBag.Categories = movieContext.categories.ToList();
+            ViewBag.Editing = false;
             return View();
         }
 
@@ -45,16 +46,62 @@ namespace IS413Mission5_Movies.Controllers
             {
                 movieContext.Add(movie);
                 movieContext.SaveChanges();
-                List<Movie> movies = movieContext.movies.Include(m => m.Category).ToList();
                 // this calls the index function (goes back to the movie collection list)
-                return View("Index", movies);
+                return RedirectToAction("Index");
             }
-            else return View();
+            else
+            {
+                ViewBag.Categories = movieContext.categories.ToList();
+                ViewBag.Editing = false;
+                return View();
+            }
         }
 
         public IActionResult MyPodcasts()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult EditMovie(int movieid)
+        {
+            //reuse the add movie form as the edit form
+            ViewBag.Categories = movieContext.categories.ToList();
+            ViewBag.Editing = true;
+            var movie = movieContext.movies.Single(x => x.MovieId == movieid);
+            return View("AddMovie", movie);
+        }
+
+        [HttpPost]
+        public IActionResult EditMovie(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                movieContext.Update(movie);
+                movieContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Categories = movieContext.categories.ToList();
+                ViewBag.Editing = true;
+                return View("AddMovie");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult DeleteMovie(int movieid)
+        {
+            Movie movie = movieContext.movies.Single(m => m.MovieId == movieid);
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteMovie(Movie movie)
+        {
+            movieContext.movies.Remove(movie);
+            movieContext.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
